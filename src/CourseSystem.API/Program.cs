6a72079ae6fb1019882b332277b6a46aa81d710a
Kamal Y <KamalY@risk.az>
2025-12-09 12:51:46 +0400

    Added Swagger documentation

--- src/CourseSystem.API/Program.cs
+++ src/CourseSystem.API/Program.cs
@@ -1,10 +1,22 @@
 using CourseSystem.API.Extensions;
 using CourseSystem.Application;
 using CourseSystem.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CourseSystem API",
+        Version = "v1",
+        Description = "API documentation for the CourseSystem application."
+    });
+});
 
 builder.Services.AddExceptionHandling();
 
@@ -12,6 +24,15 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "CourseSystem API v1");
+    });
+}
 
 app.UseHttpsRedirection();
 
