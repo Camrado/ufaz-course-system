3bc365847730968af16e181c13526f589534fc80
LuisMarkusTorres <luismarkus31@gmail.com>
2025-12-10 09:24:39 +0000

    Fixed table migration on app start-up

--- src/CourseSystem.API/Program.cs
+++ src/CourseSystem.API/Program.cs
@@ -25,6 +25,12 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<YourDbContext>();
+    db.Database.Migrate();   // Apply migrations automatically
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
